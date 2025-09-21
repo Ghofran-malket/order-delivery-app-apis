91335ce46101eb232197666fe8e9333a21328a56
Ghofran Malkat <gh.malket@gmail.com>
2025-09-21 18:14:58 +0200

    install dotenv, add config to connect to monodb, make a user register route

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+const registerUser = async (req, res) => {
+    const { name, email, password, role, number } = req.body;
+
+    //check if user exist
+    const userExist = await User.findOne({ email });
+    if (userExist) return res.status(400).json({message: 'Email is already exist'});
+
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    //create the user
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: hashedpassword,
+        role: role,
+        number: number
+    });
+
+    if(user){
+        res.status(201).json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            number: user.numer
+        });
+    } else {
+        res.status(400).json({message: 'invalid user data'});
+    }
+
+}
+
+module.exports = registerUser
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 // index.js
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
 const app = express();
+connectDB();
+
+const userRoutes = require('./routes/userRoutes');
 
 app.use(express.json()); // To parse JSON bodies
 
@@ -9,5 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
