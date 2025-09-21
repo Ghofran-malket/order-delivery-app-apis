36f229456b1ff8bca912d1a5f67f51c78cb1bf2d
Ghofran Malkat <gh.malket@gmail.com>
2025-09-21 17:13:10 +0200

    install mongoose lib and add userModel

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+        match: [/.+\@.+\..+/, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password']
+    },
+    role: {
+        type: String,
+        enum: ['genie', 'customer'],
+        default: 'customer',
+        required: [true, 'Please enter your role']
+    },
+    number: {
+        type: String,
+        required: [true, 'Please enter your number']
+    },
+});
+
+const User = mongoose.Model('User', userSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // index.js
 const express = require('express');
 const app = express();
-const itemsRoutes = require('./routes/items');
 
 app.use(express.json()); // To parse JSON bodies
-app.use('/api/items', itemsRoutes); // Use item routes
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
